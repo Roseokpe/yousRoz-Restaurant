dd79a889df4c8d9120f3eeb5526e27e4dce7c920
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-07-26 20:04:32 +0430

    Fix the webpack errors

--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,32 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  mode: 'development',
+  devServer: {
+    static: './dist',
   },
-
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
     ],
   },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: "/webpack-demo/",
   },
-
+  // type: 'module',
+  // optimization: {
+  //     runtimeChunk: 'single',
+  // },
 };
+// webpack file
