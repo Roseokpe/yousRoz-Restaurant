9395cc3d5b18ef87c9e648d1d00e4bbfe9257f26
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-07-26 21:29:03 +0430

    fix linters errors

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: "/webpack-demo/",
+    publicPath: '/webpack-demo/',
   },
   // type: 'module',
   // optimization: {
