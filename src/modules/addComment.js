e18f7f54a98ccc3bfef0e2a0dc375b31debd0166
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-07-27 20:54:33 +0430

    done with comments ui

---
+++ src/modules/addComment.js
@@ -0,0 +1,9 @@
+export default (comments =>{
+  const commentSec = document.getElementById ('display-comment')
+  const input = document.getElementById ('comment-input');
+  const inputValue = input.value;
+  
+  commentSec.innerText = (inputValue);
+  getAll = () => comments
+}) 
+
